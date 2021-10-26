e760c3e5365f1be557ece199d8cda087fe243e8f
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-10-26 21:42:37 +0200

    test i zaczolem program chyba nwm

--- src/Zadanie2.java
+++ src/Zadanie2.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Zadanie2 {
 
-    public static void main(String []args)
-    {
-        System.out.println("Hello Wo0rld!");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t;
+        t = scan.nextInt();
+        if (t > 100) System.exit(0);
+        if (t < 0) System.exit(0);
+        for (int i = 0; i < t; i++) {
+            System.out.println("Siema");
+        }
+
+
     }
 }
