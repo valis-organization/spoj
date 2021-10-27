e9ca97aed44824044e95bc128b3335e5c01536e5
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-10-27 23:47:44 +0200

    Starting the code when im in champselect

--- src/Prime1.java
+++ src/Prime1.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
+
 /***
  * https://www.spoj.com/problems/PRIME1/
  */
 public class Prime1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t;
+        t = scan.nextInt();
+        if (t <= 10) {
+            for (int i = 0; i < t; i++) {
+                int m = scan.nextInt();
+                int n = scan.nextInt();
+                System.out.println(m + " " + n);
+            }
+        }
+
+
+    }
 }
