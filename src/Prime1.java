42231d31ea48790070859ae4d89ef2d43d6f203e
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-10-30 01:33:35 +0200

    Wykreslanie do 7, jestem blisko ale musze cos zmienic jeszcze

--- src/Prime1.java
+++ src/Prime1.java
@@ -12,10 +12,28 @@ public class Prime1 {
             for (int i = 0; i < t; i++) {
                 int m = scan.nextInt();
                 int n = scan.nextInt();
-                System.out.println(m + " " + n);
+                if (1 <= m && m <= n && n - m <= 1000000 && n <= 1000000000) {
+                    for (int p = m; p <= n; p++) {
+                        if (p != 1) {
+                            if (p % 2 != 0 || p == 2) {
+                                if (p % 3 != 0 || p == 3) {
+                                    if (p % 5 != 0 || p == 5) {
+                                        if (p % 7 != 0 || p == 7) {
+                                            System.out.println(p);
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+
             }
+
         }
-
-
     }
 }
