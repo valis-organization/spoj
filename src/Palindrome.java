3508a46549aa3a17448f0efc357cd9b2c2a2e7ac
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-02 22:19:48 +0100

    Nowe zadanie - https://www.spoj.com/problems/PALIN/ PALIN - The Next Palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+//RAFALCHAMPIK
+
+/***
+ * https://www.spoj.com/problems/PALIN/
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
