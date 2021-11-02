14ac1fae62c1365bba394023f201c694fb718f6b
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-02 14:10:55 +0100

    Zrobione zadanie (chyba)

--- src/DivisorSummation.java
+++ src/DivisorSummation.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 //RAFALCHAMPIK
+
 /***
  * https://www.spoj.com/problems/DIVSUM/
  */
 
 public class DivisorSummation {
 
-    public static void main(String[]args){
-            System.out.println("Hello World!");
+    public static void main(String[] args) {
+        int testy, liczba, suma = 0;
+        Scanner scan = new Scanner(System.in);
+        testy = scan.nextInt();
+        for (int i = 0; i < testy; i++) {
+            liczba = scan.nextInt();
+            for (int j = 1; j < liczba; j++) {
+                if (liczba % j == 0) {
+                    suma += j;
 
+                }
+            }
+            System.out.println(suma);
+            suma = 0;
+        }
     }
 }
+
