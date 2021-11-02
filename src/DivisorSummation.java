ca27c9c0751b1a547b13e9803fd5cccb8d4d1f50
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-02 01:12:01 +0100

    Nowe zadanie - https://www.spoj.com/problems/DIVSUM/

---
+++ src/DivisorSummation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//RAFALCHAMPIK
+/***
+ * https://www.spoj.com/problems/DIVSUM/
+ */
+
+public class DivisorSummation {
+
+    public static void main(String[]args){
+            System.out.println("Hello World!");
+
+    }
+}
