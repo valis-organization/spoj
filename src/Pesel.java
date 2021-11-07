585d4734042bfe656903bdcab3b489cb4b1bdbb6
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-07 01:23:39 +0100

    Stworzenie nowego zadania https://pl.spoj.com/problems/JPESEL/

---
+++ src/Pesel.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//RAFALCHAMPIK
+
+/***
+ * https://pl.spoj.com/problems/JPESEL/
+ */
+
+public class Pesel {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+    }
+}
