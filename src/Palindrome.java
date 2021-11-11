d1b2971dcedbedb953f08628319549f305956c7d
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-11 20:44:28 +0100

    Nie dziala

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -7,10 +7,13 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int testCount;
+        int testCount, asd;
         testCount = scan.nextInt();
 
-        for(int i=0; i<testCount; i++){
+        for (int i = 0; i < testCount; i++) {
+            asd = scan.nextInt();
+            System.out.println("Jednosci: " + asd % 10);
+            System.out.println("Dziesiatki: " + ((asd % 100) - (asd % 10)) / 10);
 
 
         }
--- src/Pesel.java
+++ src/Pesel.java
@@ -6,8 +6,23 @@ import java.util.Scanner;
  */
 
 public class Pesel {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCount;
+        String peselNumbers;
+        testCount = scan.nextInt();
 
+        for (int i = 0; i <= testCount; i++) {
+            peselNumbers = scan.nextLine();
+            char[] convert = peselNumbers.toCharArray();
+        //    for(int j= 0; j< convert.length;j++){
+          //      System.out.println(convert);
+            //}
+            int suma = convert[0] + (convert[1] * 3) + convert[2] * 7 + convert[3] * 9 + convert[4] + convert[5] * 3 + convert[6] * 7 + convert[7] * 9 + convert[8] + convert[9] * 3 + convert[10];
+            System.out.println(suma);
+        }
     }
+
 }
+
+
