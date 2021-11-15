68140ca08e02e180f0389913fc5a1362be0265f3
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-15 14:06:46 +0100

    Program rysuje plansze

--- src/Plansza.java
+++ src/Plansza.java
@@ -1,7 +1,19 @@
 public class Plansza {
-    public static void main(String [] args){
 
-        System.out.println("Hello World!");
+    static int board_Y = 10;
+    static int board_X = 5;
+
+    public static void main(String[] args) {
+
+        String board[][] = new String[board_X][board_Y];
+        for (int i = 0; i < board_X; i++) {
+            System.out.println("");
+            for (int j = 0; j < board_Y; j++) {
+                board[i][j] = "X";
+                System.out.print(board[i][j]);
+
+            }
+        }
     }
 
 }
