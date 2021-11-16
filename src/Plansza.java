966abf62ba6b5956da65634c3460c4c799d14123
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-16 11:54:01 +0100

    Ustawienie poczatkowej pozycji playera

--- src/Plansza.java
+++ src/Plansza.java
@@ -1,19 +1,40 @@
+import java.util.Scanner;
+
 public class Plansza {
 
     static int board_Y = 10;
     static int board_X = 5;
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String key;
 
         String board[][] = new String[board_X][board_Y];
-        for (int i = 0; i < board_X; i++) {
-            System.out.println("");
-            for (int j = 0; j < board_Y; j++) {
-                board[i][j] = "X";
-                System.out.print(board[i][j]);
+        int playerPosX = board_X - 5;
+        int playerPosY = board_Y - 1;
+        board[playerPosX][playerPosY] = "O"; // Pozycja poczatkowa
+       // do {
+            for (int i = 0; i < board_X; i++) {
+                System.out.println("");
+                for (int j = 0; j < board_Y; j++) {
+                    if(board[playerPosX][playerPosY] == board[i][j]){
+                        board[i][j]= "O";
+                    System.out.print(board[i][j]);}
+                    else{
+                        board[i][j] = "X";
+                    System.out.print(board[i][j]);}
 
+                }
             }
-        }
+            key = scan.nextLine();
+        //}while(key == "W" || key == "A" || key == "S" || key == "D");
+          /*          String key = scan.nextLine();
+            if (key == "W"){
+                playerPosY++;
+            }
+            if(key== "S"){
+                playerPosY--;
+   */
     }
 
 }
