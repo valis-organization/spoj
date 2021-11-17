c59222fcc179f9940759c174820c5a87a0c361b1
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-17 14:08:36 +0100

    Program wstepnie dziala. Musze jeszcze zmienic sterowanie na 'WSAD' z '1234' xD i zaimplementowac funkcje

--- src/Plansza.java
+++ src/Plansza.java
@@ -7,34 +7,61 @@ public class Plansza {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String key;
+        int key;
 
         String board[][] = new String[board_X][board_Y];
-        int playerPosX = board_X - 5;
-        int playerPosY = board_Y - 1;
-        board[playerPosX][playerPosY] = "O"; // Pozycja poczatkowa
-       // do {
+        int playerPosX = (board_X - 5);
+        int playerPosY = (board_Y - 1);
+        int previousPosX, previousPosY;
+
+        do {
+            for (int m = 0; m < 5; m++) {
+                System.out.println("\n");
+            }
+            previousPosX = playerPosX;
+            previousPosY = playerPosY;
+            board[playerPosX][playerPosY] = "O"; // Pozycja poczatkowa
+
             for (int i = 0; i < board_X; i++) {
                 System.out.println("");
                 for (int j = 0; j < board_Y; j++) {
-                    if(board[playerPosX][playerPosY] == board[i][j]){
-                        board[i][j]= "O";
-                    System.out.print(board[i][j]);}
-                    else{
+                    if (board[playerPosX][playerPosY] == board[i][j]) {
+                        board[i][j] = "O";
+                        System.out.print(board[i][j]);
+                    } else {
                         board[i][j] = "X";
-                    System.out.print(board[i][j]);}
+                        System.out.print(board[i][j]);
+                    }
 
                 }
             }
-            key = scan.nextLine();
-        //}while(key == "W" || key == "A" || key == "S" || key == "D");
-          /*          String key = scan.nextLine();
-            if (key == "W"){
+            key = scan.nextInt();
+            board[previousPosX][previousPosY] = "X"; // Pozycja poprzednia
+            if (key == 1) {
+                playerPosY --;
+            }
+            if (key == 2) {
                 playerPosY++;
             }
-            if(key== "S"){
-                playerPosY--;
-   */
-    }
+            if(key == 3){
+                playerPosX++;
+            }
+            if(key==4){
+                playerPosX--;
+            }
 
+            if(playerPosX>=5)
+                playerPosX--;
+            if(playerPosY>=10)
+                playerPosY--;
+            if(playerPosX<0)
+                playerPosX++;
+            if(playerPosY<0)
+                playerPosY++;
+
+        }while (key == 1 || key == 2 || key == 3 || key == 4) ;
+
+
+
+    }
 }
