104b2207c0521914868a27f943074df4e0f709c0
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-11-17 21:13:05 +0100

    Zmienienie sterowania na WSAD

--- src/Plansza.java
+++ src/Plansza.java
@@ -6,8 +6,13 @@ public class Plansza {
     static int board_X = 5;
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int key;
+        char key;
+   /*     Thread thread = new Thread(() -> {
+
+        });
+        thread.start();
+     */   Scanner scan = new Scanner(System.in);
+
 
         String board[][] = new String[board_X][board_Y];
         int playerPosX = (board_X - 5);
@@ -35,32 +40,32 @@ public class Plansza {
 
                 }
             }
-            key = scan.nextInt();
+            key = scan.next().charAt(0);
             board[previousPosX][previousPosY] = "X"; // Pozycja poprzednia
-            if (key == 1) {   // Chodzenie (poki co 4 = Gora, 3 = Dol, 2 = Prawo, 1 = Lewo
+            if (key == 'A' || key == 'a') {   // Chodzenie (poki co 4 = Gora, 3 = Dol, 2 = Prawo, 1 = Lewo
                 playerPosY --;
             }
-            if (key == 2) {
+            if (key == 'D' || key =='d') {
                 playerPosY++;
             }
-            if(key == 3){
+            if(key == 'S' || key == 's'){
                 playerPosX++;
             }
-            if(key==4){
+            if(key=='W' || key == 'w'){
                 playerPosX--;
             }
 
 
-            if(playerPosX>=5) //Nie wychodzi poza granice planszy
+            if(playerPosX>=board_X) //Nie wychodzi poza granice planszy
                 playerPosX--;
-            if(playerPosY>=10)
+            if(playerPosY>=board_Y)
                 playerPosY--;
             if(playerPosX<0)
                 playerPosX++;
             if(playerPosY<0)
                 playerPosY++;
 
-        }while (key == 1 || key == 2 || key == 3 || key == 4) ;
+        }while (true) ;
 
 
 
