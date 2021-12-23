4857f07b8f020c453145bd65afaaf52589553b9c
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2021-12-23 17:37:26 +0100

    Dodanie zadania i ukonczenie go

---
+++ src/StringMerge.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/***
+ * https://pl.spoj.com/problems/PP0504B/
+ */
+
+public class StringMerge {
+
+
+    static char[] firstStringToCharArray(String firstRow) {
+        char[] convertedFirst = firstRow.toCharArray(); // konwertowanie pierwszego ciagu do chara
+        return convertedFirst;
+    }
+
+    static char[] secondStringToCharArray(String secondRow) {
+        char[] convertedSecond = secondRow.toCharArray(); // konwertowanie drugiego ciagu do chara
+        return convertedSecond;
+    }
+
+    static boolean isFirstRowLongerThanSecond(String firstRow, String secondRow) {
+        int secondLength = secondRow.length();
+        int firstLength = firstRow.length();
+        if (firstLength > secondLength)
+            return true;
+        else
+            return false;
+    }
+
+    static void string_merge(String firstRow, String secondRow) {
+        char[] convertedFirst = firstStringToCharArray(firstRow);
+        char[] convertedSecond = secondStringToCharArray(secondRow);
+        if (isFirstRowLongerThanSecond(firstRow, secondRow)) {
+            for (int i = 0; i < secondRow.length(); i++) {
+                System.out.print(convertedFirst[i]);
+                System.out.print(convertedSecond[i]);
+            }
+        } else {
+            for (int i = 0; i < firstRow.length(); i++) {
+                System.out.print(convertedFirst[i]);
+                System.out.print(convertedSecond[i]);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCount;
+        String firstRow, secondRow;
+        testCount = scan.nextInt();
+        for (int i = 0; i < testCount; i++) {
+            firstRow = scan.next();
+            secondRow = scan.next();
+            string_merge(firstRow,secondRow);
+            System.out.println();
+        }
+    }
+
+
+}
