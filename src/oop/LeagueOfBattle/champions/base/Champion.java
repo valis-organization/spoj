9dd1340762ef49b0cddb3fb793948466aed8ee2a
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2022-04-06 18:23:47 +0200

    Naprawa logiki armor penetration

--- src/oop/LeagueOfBattle/champions/base/Champion.java
+++ src/oop/LeagueOfBattle/champions/base/Champion.java
@@ -30,8 +30,8 @@ public abstract class Champion implements Enemy, SpellProvider {
     }
 
     public final void receiveSpell(Description description) {
-        int relativeArmor = (int) ((armor * (1 - armorPenetration)) / 2); //TODO Naprawic bug, ktory polega na braniu armorPen od atakowanego championa
-        int relativeMR = (int) ((magicResist * (1 - magicPenetration)) / 2);
+        int relativeArmor = (int) ((armor * (1 - armorPenetration * 0.01)) / 2); //TODO Naprawic bug, ktory polega na braniu armorPen od atakowanego championa
+        int relativeMR = (int) ((magicResist * (1 - magicPenetration * 0.01)) / 2);
         relativeArmor = relativeArmor == 0 ? 1 : relativeArmor;
         relativeMR = relativeMR == 0 ? 1 : relativeMR;
 
