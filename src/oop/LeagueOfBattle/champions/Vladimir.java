2d15eea44e5f7ca1aeaee92018638e8172dc7508
NiezlyPajacPL <poradnikidomajkraft@gmail.com>
2022-05-16 13:02:17 +0200

    Stworzenie klasy vladimira

--- src/oop/LeagueOfBattle/champions/Vladimir.java
+++ src/oop/LeagueOfBattle/champions/Vladimir.java
@@ -1,46 +1,62 @@
-/* package oop.LeagueOfBattle.champions;
+package oop.LeagueOfBattle.champions;
 
 import oop.LeagueOfBattle.champions.base.Champion;
 import oop.LeagueOfBattle.champions.base.Enemy;
 import oop.LeagueOfBattle.champions.base.spell.Description;
+import oop.LeagueOfBattle.champions.base.spell.Spell;
+import oop.LeagueOfBattle.helpers.MathHelper;
+import oop.LeagueOfBattle.menagers.ChampionVoiceLineHandler;
 
 public class Vladimir extends Champion {
-    private final String NAME = "Vladimir";
 
-    public Vladimir() {
-        name = NAME;
+    private final String VLADIMIR = "Vladimir";
+
+    public Vladimir(ChampionVoiceLineHandler voiceHandler) {
+        super(voiceHandler);
+        name = VLADIMIR;
         maxHP = 300;
         hp = 300;
         armor = 20;
         magicResist = 20;
-        abilityPower = 0;
-        attackDimig = 40;
+        abilityPower = 40;
+        attackDimig = 10;
         actionPoints = 4;
+        armorPenetration = 0;
         currentActionPoints = actionPoints;
+        costQ = 2;
+        costW = 1;
+        costE = 2;
+        costR = actionPoints;
         isAssassin = false;
     }
 
     @Override
-    public Description useQ(Enemy enemy) {
-        return null;
+    public Spell provideAA(Enemy enemy) {
+        Description AA = new Description(0, attackDimig, 0, 0, 0, 0, false);
+        return new Spell(AA, 1);
     }
 
     @Override
-    public Description useW(Enemy enemy) {
-        return null;
+    public Spell provideQ(Enemy enemy) {
+        return new Spell(new Description(), 2);
     }
 
     @Override
-    public Description useE(Enemy enemy) {
-        return null;
+    public Spell provideW(Enemy enemy) {
+        return new Spell(new Description(), 2);
     }
 
     @Override
-    public Description useR(Enemy enemy) {
-        return null;
+    public Spell provideE(Enemy enemy) {
+        return new Spell(new Description(), 2);
     }
 
- /*   @Override
+    @Override
+    public Spell provideR(Enemy enemy) {
+        return new Spell(new Description(), 2);
+    }
+}
+/*   @Override
     public void spellQ(Champion champion) {
         if (!isSpellOnCooldown[0]) {
             if (currentActionPoints >= 2) {
---
+++ src/oop/LeagueOfBattle/voiceLines/vladimir/VladimirVoiceHandler.java
@@ -0,0 +1,70 @@
+package oop.LeagueOfBattle.voiceLines.vladimir;
+
+import oop.LeagueOfBattle.helpers.MathHelper;
+import oop.LeagueOfBattle.menagers.ChampionVoiceLineHandler;
+import oop.LeagueOfBattle.voiceLines.SoundHandler;
+
+public class VladimirVoiceHandler implements ChampionVoiceLineHandler {
+    private final SoundHandler soundHandler;
+    public static String Q1 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\Q1.wav";
+    public static String Q2 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\Q2.wav";
+    public static String MQ1 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\MarkedQ1.wav";
+    public static String MQ2 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\MarkedQ2.wav";
+    public static String W1 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\W1.wav";
+    public static String W2 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\W2.wav";
+    public static String W3 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\W3.wav";
+    public static String W4 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\W4.wav";
+    public static String W5 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\W5.wav";
+    public static String E1 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\E1.wav";
+    public static String R1 = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\R1.wav";
+    public static String Pick = "src\\oop\\LeagueOfBattle\\voiceLines\\ryze\\Pick.wav";
+
+    public VladimirVoiceHandler(SoundHandler soundHandler) {
+        this.soundHandler = soundHandler;
+    }
+
+    @Override
+    public void playPickingSound() {
+        soundHandler.playSound(Pick);
+    }
+
+    @Override
+    public void playQSound() {
+        int rand = MathHelper.randomInt(1, 4);
+        if (rand == 1) {
+            soundHandler.playSound(Q1);
+        } else if (rand == 2) {
+            soundHandler.playSound(Q2);
+        } else if (rand == 3) {
+            soundHandler.playSound(MQ1);
+        } else if (rand == 4) {
+            soundHandler.playSound(MQ2);
+        }
+    }
+    @Override
+    public void playWSound() {
+        int rand = MathHelper.randomInt(1, 5);
+        if (rand == 1) {
+            soundHandler.playSound(W1);
+        } else if (rand == 2) {
+            soundHandler.playSound(W2);
+        } else if (rand == 3) {
+            soundHandler.playSound(W3);
+        } else if (rand == 4) {
+            soundHandler.playSound(W4);
+        } else if (rand == 5) {
+            soundHandler.playSound(W5);
+        }
+    }
+
+    @Override
+    public void playESound() {
+        soundHandler.playSound(E1);
+    }
+
+    @Override
+    public void playRSound() {
+        soundHandler.playSound(R1);
+    }
+}
+
